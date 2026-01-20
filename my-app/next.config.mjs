86cf393a2d25ebb8b1bc447bdc0ceffa7ff07e0b
qinyj12030 <qinyj12030@163.com>
2026-01-20 16:52:57 +0800

    更新文档结构，添加新手教程和信息管理模块内容

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -5,6 +5,10 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  allowedDevOrigins: [
+    'http://192.168.50.80:3000',
+    '192.168.50.80'
+  ],
 };
 
 export default withMDX(config);
